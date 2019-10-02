2ef8470038cf5fe683bff685d1bce5137aa5c7ab
Guido Hernani <guimega.gh@gmail.com>
2019-10-02 11:52:16 -0400

    Ejemplo inicial

--- src/main/java/com/pumachat/chatbot/ChatbotApplication.java
+++ src/main/java/com/pumachat/chatbot/ChatbotApplication.java
@@ -1,13 +1,26 @@
 package com.pumachat.chatbot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class ChatbotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatbotApplication.class, args);
+		// Se inicializa el contexto de la API
+		ApiContextInitializer.init();
+
+		// Se crea un nuevo Bot API
+		final TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+		try {
+			// Se registra el bot
+			telegramBotsApi.registerBot(new EchoBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }

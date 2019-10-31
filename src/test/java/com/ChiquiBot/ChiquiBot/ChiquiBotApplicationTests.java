0a89686469015a9640acfb7c278b9409773debc2
Guido Hernani <guimega.gh@gmail.com>
2019-10-30 23:33:59 -0400

    Reconstruccion del Bot con base de EchoBot

---
+++ src/test/java/com/ChiquiBot/ChiquiBot/ChiquiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChiquiBot.ChiquiBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiquiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

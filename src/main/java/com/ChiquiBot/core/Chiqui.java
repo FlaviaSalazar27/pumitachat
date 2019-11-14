65c58799fa09fdb32ee1737a7c8a50da7603268d
Guido Hernani <guimega.gh@gmail.com>
2019-11-14 00:52:02 -0400

    commit para hacer merge

--- src/main/java/com/ChiquiBot/core/Chiqui.java
+++ src/main/java/com/ChiquiBot/core/Chiqui.java
@@ -1,4 +1,5 @@
 package com.ChiquiBot.core;
+import org.slf4j.ILoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -7,23 +8,22 @@ import org.telegram.telegrambots.meta.api.objects.File;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.logging.Logger;
+
 public class Chiqui extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(final Update update) {
         // Esta función se invocará cuando nuestro bot reciba un mensaje
        /* File doc = new File();
-        doc.getFileUrl("834962965:AAG0S_TVvZrGtzE5hYVGbpSevfoiAGsuy7k","http://www.lapazbus.bo/fileman/Uploads/files/pdf/Mapa%20Oficial%20Achumani.pdf");
-        */// Se obtiene el mensaje escrito por el usuario
-
-
+        */
         final String messageTextReceived = update.getMessage().getText();
 
         // Se obtiene el id de chat del usuario
         final long chatId = update.getMessage().getChatId();
         final String nombre = update.getMessage().getFrom().getFirstName();
         String mensaje ="http://amn.bo/wp-content/uploads/2019/08/69112080_1638670066269349_8967221364142374912_n.jpg";
-        // Se crea un objeto mensaje
+        
         SendDocument message = new SendDocument().setChatId(chatId).setDocument("http://www.lapazbus.bo/fileman/Uploads/files/pdf/mapa_rutachasquipampa.pdf");
 
         try {
